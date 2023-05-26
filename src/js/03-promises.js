f855aefa72c99f545067d5ce75d83030b522c89e
Chicca1 <una_chicca@ukr.net>
2023-05-26 23:26:28 +0300

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,43 +1,42 @@
-const form = document.querySelector('.form');
-form.addEventListener('submit', handleSubmit);
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates, dateStr, instance) {
-    console.log(selectedDates[0]);
-    const startButton = document.querySelector('[data-start]');
-    startButton.disabled = selectedDates[0] <= new Date();
-  },
-};
-
-
-function handleSubmit(event) {
-  event.preventDefault(); 
-
-  const delayInput = document.querySelector('input[name="delay"]');
-  const stepInput = document.querySelector('input[name="step"]');
-  const amountInput = document.querySelector('input[name="amount"]');
-
-  const delay = Number(delayInput.value);
-  const step = Number(stepInput.value);
-  const amount = Number(amountInput.value);
-
-  
-  for (let i = 1; i <= amount; i++) {
-    const position = i;
-    const promiseDelay = delay + (i - 1) * step;
-
-    createPromise(position, promiseDelay)
-      .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-  }
-
-  form.reset(); 
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+  const createBtn = document.querySelector('#createBtn');
+
+  createBtn.addEventListener('click', () => {
+    const delayInput = document.querySelector('input[name="delay"]');
+    const stepInput = document.querySelector('input[name="step"]');
+    const amountInput = document.querySelector('input[name="amount"]');
+
+    const delay = parseInt(delayInput.value);
+    const step = parseInt(stepInput.value);
+    const amount = parseInt(amountInput.value);
+
+    for (let i = 0; i < amount; i++) {
+      createPromise(i, delay + i * step)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+
+   
+    delayInput.value = '';
+    stepInput.value = '';
+    amountInput.value = '';
+  });
+});
